7d110120a4ae45453b8dcc53d693cd75d4791291
archiee-exe <sn.archie22@gmail.com>
2025-05-22 18:39:10 -0700

    tut 09

--- tutorials/09-web-audio/js/sound.js
+++ tutorials/09-web-audio/js/sound.js
@@ -1,9 +1,22 @@
 let song = document.querySelector("#song");
 
 let playBtn = document.querySelector("#play-button");
+let stopBtn = document.querySelector("#stop-button");
 
 playBtn.addEventListener("click", function(){
+
    song.play();  
 console.log("play")})
 
+stopBtn.addEventListener("click", function(){
+song.pause();
+console.log("stop")})
 
+let volumeControl = document.getElementById('volume');
+  
+  song.volume = volumeControl.value / 100;
+
+  // Update volume when slider changes
+  volumeControl.addEventListener('input', () => {
+    song.volume = volumeControl.value / 100;
+  });
